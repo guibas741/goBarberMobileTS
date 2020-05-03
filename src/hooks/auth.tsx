1013216705f4d9807ead04da31b3ce41afee2f75
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-05-03 13:13:46 -0300

    Creates App routes and Auth routes and add navigation between routes stacks

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -21,12 +21,14 @@ interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  loading: boolean;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -38,6 +40,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+
+      setLoading(false);
     }
 
     loadStoragedData();
@@ -66,7 +70,7 @@ export const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut, loading }}>
       {children}
     </AuthContext.Provider>
   );
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Button from '../../components/Button';
+
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Button onPress={signOut}>Sair</Button>
+    </View>
+  );
+};
+
+export default Dashboard;
