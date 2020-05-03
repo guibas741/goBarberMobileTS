1015c2b4206899d5931d0a0646f57cfda2555473
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-05-02 21:08:24 -0300

    Creates Input and Button components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Image } from 'react-native';
-import { Container, Title } from './styles';
 import logoImage from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImage} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('deu');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
