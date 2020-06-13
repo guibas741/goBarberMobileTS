881812fd02d601a9e69ec59587577bb0d9de9db9
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-06-13 11:19:53 -0300

    Creates Dashboard header

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,9 +8,15 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 interface SignInCredentials {
   email: string;
@@ -18,7 +24,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
   loading: boolean;
